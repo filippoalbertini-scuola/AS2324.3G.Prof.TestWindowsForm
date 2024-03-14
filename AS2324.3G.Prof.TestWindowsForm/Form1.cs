52fcc3763452cae69c59494cd7f05b7e179c45c9
Filippo Albertini <filippo@filippoalbertini.it>
2024-03-14 09:02:24 +0100

    feat: aggiunta gestione colorazione label in base al sesso

--- AS2324.3G.Prof.TestWindowsForm/Form1.cs
+++ AS2324.3G.Prof.TestWindowsForm/Form1.cs
@@ -15,6 +15,12 @@ namespace AS2324._3G.Prof.TestWindowsForm
         private void btnSaluta_Click(object sender, EventArgs e)
         {
             lblSaluto.Text = "Ciao " + txtNome.Text;
+
+            // colora la label in base al "sesso" dell'utente
+            if (txtNome.Text.EndsWith("a"))
+                lblSaluto.ForeColor = Color.Pink;
+            else
+                lblSaluto.ForeColor = Color.Blue;
         }
     }
 }
